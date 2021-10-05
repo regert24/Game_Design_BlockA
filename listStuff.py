669df80d4a91bd574824913b7c6c2bad4acd7043
regert24 <tomdoesbusiness@gmail.com>
2021-10-05 09:02:46 -0500

    10-5 quiz

--- listStuff.py
+++ listStuff.py
@@ -1,10 +1,87 @@
 #Tom Reger
 # Collections in py
 
-MyFruits = ["apple", "berries", "bananas"]
+import random, os
 
-for x in MyFruits:
-    print (x)
-for x in MyFruits[0]:
-    print( "_", end="")
-    
+def updateWord(word, guesses):
+  for letter in word:
+        if letter in guesses:
+            print(letter, end=" ")
+        else:
+            print('_', end=' ')
+#define func for mmenu
+def Menu():
+    print("# instructions#")
+    print("# Menu ##")
+    print("      1. Animals             ")
+    print("      2. Fruits              ")
+    print("      3. Computer Parts      ")
+    print("      4. Exit                ")
+    sel=input("What would you like to play?")
+    sel=int(sel)
+    return sel
+def selWord(sel):
+    if sel == 1:
+        word=random.choice(animals)
+    elif sel ==2:
+        word=random.choice(myfruits)
+    else:
+        word=random.choice(compParts)
+
+animals=["tiger, elephant"]
+myfruits = ["apple","bananas","oranges","grapes","strawberries","blackberries","blackberries"]
+compParts=["keyboard, computer"]
+
+
+print("Word Game")
+print("Guess what fruit I am thinking of")
+name=input("What is your name?")
+counter=0
+sel=Menu()
+while sel !=4:
+    print(name, "good luck, you have 5 chances")
+    turns=5
+    counter +=1
+    word= random.choice(myfruits)
+    word=word.lower()
+    print(word) #revove later
+    guesses=''
+    updateWord(word, guesses)
+    while turns>0:
+        newguess=input("Give me a letter")
+        newguess=newguess.lower()
+        if newguess in word:
+            guesses += newguess
+            print("You guessed right")
+        else:
+            turns -=1
+            print ("Sorry you have", turns, "left")
+answer=input(name+ "do you want to play again")
+    sel=Menu()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for x in MyFruits:
+#     print (x)
+# for x in MyFruits[0]:
+#     print( "_", end="")
---
+++ quiz10-5.py
@@ -0,0 +1,21 @@
+import os
+import random 
+os.system('cls')
+
+#Question 1
+print(len('Hello World!'))
+#Question 2
+strString='Study.com is a wonderful resource.'
+print(strString.find('great'))
+print(strString.find('wonderful'))
+print(strString.find('.'))
+#Question 3
+print(strString[15:24])
+#Question 5
+print(strString[15:24].find('der')+len(strString))
+#Question 6
+var1=17
+var2=4
+result=var1%var2
+#Quiz End
+
