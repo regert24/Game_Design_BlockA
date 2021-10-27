a2ce29f55dad202379cfcdcd907b8dc572e3f626
regert24 <tomdoesbusiness@gmail.com>
2021-10-27 09:07:33 -0500

    menu 10/27

--- windowspygamefonts.py
+++ windowspygamefonts.py
@@ -13,12 +13,18 @@ py.display.set_caption("Setting window")
 
 #TITLE_FONT=py.font.SysFont(name, size, bold=False,italic=False)
 TITLE_FONT=py.font.SysFont('comicsans', 80)
-SubTitle=py.font.SysFont('comicsans',40, italic=True )
+subtitle_font=py.font.SysFont('comicsans',40, italic=True )
 def display_message(message):
     py.time.delay(100)
     text= TITLE_FONT.render(message,1,BLACK)
-    win.blit(text, (width/2-text.get_width()/2, height/2-text.get_height()/2))
+    win.blit(text, (width/2-text.get_width()/2, 30))
     py.display.update()
+    py.time.delay(100)
+def display_subtitle(message, x, y):
+    py.time.delay(100)
+    text = subtitle_font.render(message, 1,BLACK)
+    win.blit(text, (x, y))
+    py.display.flip()
     py.time.delay(100)
 run=True
 while run:
@@ -29,13 +35,10 @@ while run:
     display_message("settings")
     py.time.delay(300)
     win.fill((0,0,0))
-    display_message("oops")
-    py.time.delay(300)
-    win.fill((0,0,0))
-    display_message("test")
-    py.time.delay(200)
-    win.fill((0,0,0))
-py.quit()
+    display_subtitle("window color", 90, 250)
+    display_subtitle("sound", 90, 350)
+    display_subtitle("quit game", 90, 450)
+#py.quit()
 
 
 
