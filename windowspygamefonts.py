3aa29811baf85033bab2ef34d6cdd3592bac14bf
regert24 <tomdoesbusiness@gmail.com>
2021-11-10 09:06:45 -0600

    11/10

--- windowspygamefonts.py
+++ windowspygamefonts.py
@@ -7,6 +7,7 @@ os.system('cls')
 pygame.init()
 py=pygame
 #Colors and their values
+
 red=(255,0,0)
 green=(0,255,0) 
 blue=(0,0,255)
@@ -16,9 +17,10 @@ black = (0,0,0)
 white=(255,255,255)
 width=800
 height = 800
-
+COLOR=white
+flag=False
 Setting_messages1 = ["SCREEN SIZE", "BACKGROUND COLOR","OBJECT COLOR","SOUNDS (ON/OFF)"]    #Setting Messages
-
+Menu_messages=["Instructions", "Level 1", "Level 2", "Settings", "Scoreboard", "Exit"]
 win = pygame.display.set_mode((width,height))
 win.fill(blue)
 pygame.display.set_caption("Setting Window")
@@ -32,22 +34,22 @@ hbox = 25
 wbox = 25
 square = pygame.Rect(10,10, wbox, hbox)
 
-def displayTitle(title):    #Title print function
+def displayTitle(title, yt):    #Title print function
     pygame.time.delay(100)
     text = title_font.render(title,1,red)
     #win.blit(text,(width/2-text.get_width()/2, height/2-text.get_height()/2))
-    win.blit(text,(width/2-text.get_width()/2,30))
+    win.blit(text,(width/2-text.get_width()/2,yt))
     pygame.display.update()
     pygame.time.delay(100)
 
-def displayMenu():  #Subtitle print function
+def displayMenu(message):  #Subtitle print function
     pygame.time.delay(100)
     x = 70
     y = 190
     square.x = x
     square.y = y
-    for i in range (0, len(Setting_messages1)):
-        word = Setting_messages1[i]
+    for i in range (0, len(message)):
+        word = message[i]
         pygame.draw.rect(win, purple, square)
         text = subtitle_font.render(word,1,red)
         win.blit(text,(x+wbox+10, y-10))
@@ -55,9 +57,16 @@ def displayMenu():  #Subtitle print function
         pygame.time.delay(100)
         y +=100
         square.y = y
+Jumping=False
+jumpCount=10
+
+
+
 
 run=True
 count = 0
+displayTitle("Menu", 40)
+displayMenu(Menu_messages)
 while run:
     pygame.time.delay(10)
     for eve in pygame.event.get():
@@ -67,41 +76,63 @@ while run:
         mouse_pressed=py.mouse.get_pressed()
         if mouse_pressed[0]:
             mouse_pos=py.mouse.get_pos()
-            if square.collidepoint(mouse_pressed):
+            if square.collidepoint(mouse_pos):
                 win.fill(green)
         
-    if count == 0:
-        displayTitle("SETTINGS")
-        displayMenu()
-        count+=1
-    mouse_pressed=py.mouse.get_pressed()
-    mouse_pos=py.mouse.get_pos()
-    y_min=190
-    y_max=212
+        mouse_pressed=py.mouse.get_pressed()
+        mouse_pos=py.mouse.get_pos()
+        #print(mouse_pos)
+        y_min=190
+        y_max=212
 
-    y_min2=291
-    y_max2=312
+        y_min2=291
+        y_max2=312
 
-    y_min3=390
-    y_max3=413
+        y_min3=390
+        y_max3=413
 
-    y_min4=491
-    y_max4=514
-    if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min and mouse_pos[1]<=y_max:
-        if mouse_pressed:
-            win.fill(blue)
-    if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min2 and mouse_pos[1]<=y_max2:
-        if mouse_pressed:
-            win.fill(blue)
-    if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min3 and mouse_pos[1]<=y_max3:
-       if mouse_pressed:
-            win.fill(blue)
-    if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min4 and mouse_pos[1]<=y_max4:
-        if mouse_pressed:
-            win.fill(blue)
+        y_min4=491
+        y_max4=514
+        if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min and mouse_pos[1]<=y_max and count==0:
+            win.fill(white)
+            displayTitle("Instructions", 40)
+            displayTitle("Back", height-100)
+            count=1
+        if mouse_pos[0]>=320 and mouse_pos[0]<=485 and mouse_pos[1]>=height-100 and mouse_pos[1]<=height and (count==1 or count==2 or count ==3 or count==5 and count==9 and count==4):
+            win.fill(white)
+            displayTitle("Menu", 40)
+            displayMenu(Menu_messages)
+            count=0
 
+
+
+           
+        #if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min2 and mouse_pos[1]<=y_max2:
+
+           
+        # if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min3 and mouse_pos[1]<=y_max3:
+         
+        if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min4 and mouse_pos[1]<=y_max4 and count==0:
+            win.fill(COLOR)
+            displayTitle("Settings", 40)
+            displayMenu(Setting_messages1)
+            displayTitle("Back", height-100)
+            count=4
+            
+        if mouse_pos[0]>=70 and mouse_pos[0]<=230 and mouse_pos[1]>=y_min4 and mouse_pos[1]<=y_max4 and count==4:
+            win.fill(COLOR)
+            displayTitle("Sound", 40)
+            displayTitle("Back", height-100)
+            count=9
+            flag=True
+            print(count)
     
-
+#count=0 main menu
+#count 1 Instructions
+#count 2 level 1  
+#count 3 level 2 
+#count 4 settings 
+#count 5 score
 
     #x,y=pygame.mouse.get_pos()
    #print("( "+ str(x)+ ", "+ str(y)+" )")
